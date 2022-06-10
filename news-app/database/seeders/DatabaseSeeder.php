9638e02a378b154bed7a90fac8b05790e09197b6
¨sara.mansori¨ <sara.mansori.amar@gmail.com>
2022-06-10 09:50:46 +0200

    add: category seeding

--- news-app/database/seeders/DatabaseSeeder.php
+++ news-app/database/seeders/DatabaseSeeder.php
@@ -18,43 +18,34 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        User::truncate();
+        // User::truncate();
 
-        $user = User::factory()->create();
+        // $user = User::factory()->create();
 
-        $economy = Category::create([
+        Category::create([
             'name' => 'Economy',
             'slug' => 'economy'
         ]);
 
-        $sports = Category::create([
+        Category::create([
             'name' => 'Sports',
             'slug' => 'sports'
         ]);
 
-        $lifestyle = Category::create([
+        Category::create([
             'name' => 'Lifestyle',
             'slug' => 'lifestyle'
         ]);
 
-        Article::factory(100)->create();
+        $articles = Article::factory(100)->create();
+        $categories = Category::all();
 
-        // $article1 = Article::create([
+        // Seed Categories
 
-        //     'title' => 'Sports and lifestyle article',
-        //     'author' => 'Anonymous',
-        //     'content' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi maxime quo doloremque soluta ab, enim asperiores excepturi exercitationem praesentium cum suscipit facilis quisquam quasi inventore molestiae debitis nesciunt aspernatur velit id ratione? Non deserunt eaque id at, molestiae deleniti sit minus totam amet facilis omnis.</p>'
-
-        // ]);
-
-        // $article1->categories()->attach([$sports->id, $lifestyle->id]);
-
-        // $article2 = Article::create([
-        //     'title' => 'Economy and lifestyle article',
-        //     'author' => $user->name,
-        //     'content' => '<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi maxime quo doloremque soluta ab, enim asperiores excepturi exercitationem praesentium cum suscipit facilis quisquam quasi inventore molestiae debitis nesciunt aspernatur velit id ratione? Non deserunt eaque id at, molestiae deleniti sit minus totam amet facilis omnis.</p>'
-        // ]);
-
-        // $article2->categories()->attach([$economy->id, $lifestyle->id]);
+        $articles->each(function ($article) use ($categories) {
+            $article->categories()->attach(
+                $categories->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
